d5487d8203871507a0b2eab27794f16646a4d763
ArunG <arung.sap@gmail.com>
2021-11-05 23:12:55 +0530

    image optimizer added to remove export error

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  images: {
+    loader: "imgix",
+    path: "https://noop/",
+},
   // Use the prefix in production and not development.
   assetPrefix: isProd ? '/lump-beta/' : '',
 }
